6537e73f85a0e169b5c1cbeb53c1c281f7056a36
antonio galluccio <antonio.galluccio@gmail.com>
2018-06-25 00:26:53 +0200

    add route to notes rest point

--- routes/notes.js
+++ routes/notes.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+/* GET notes listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+/* POST create note */
+
+/* GET note by id */
+
+/* PUT update note by id */
+
 
 module.exports = router;
