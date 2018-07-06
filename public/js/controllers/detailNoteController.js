c4ec7da140bde89160ade5ca1c12dd1962130968
galluccio <galluccio@sowatec.com>
2018-07-06 13:31:42 +0200

    add detail overview

--- public/js/controllers/detailNoteController.js
+++ public/js/controllers/detailNoteController.js
@@ -1,5 +1,28 @@
 import {restClient as client} from '../services/restClient.js';
 
 $(function() {
-  // add functionality
+  const saveNote = $("#saveNote");
+
+  class Note {
+    constructor(title, message, createdDate, rating) {
+      this.title = title;
+      this.message = message;
+      this.createdDate = createdDate;
+      //this.finished = finished;
+      //this.date_description = date_description;
+      this.rating = rating;
+      this.state = "OK";
+    }
+  }  
+
+  saveNote.click(() => {
+    const title = $("#message_title").val();
+    const message = $("#message_text").val();
+    const createdDate = $("#message_date").val();
+
+    client.createNote(new Note(title, message, createdDate, "5")).done( () => {
+      console.log("Note sent! :)");
+    });
+  });
+
 });
